82de07b27a55bf7fb1dfc195874c30a66e389079
Adam Winchell <winchell2326adam@gmail.com>
2024-04-20 14:43:44 -0500

    update to senior engineer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
     </div>
     <div class="info">
         <p>
-            I am a Software Engineer at Google on the Gmail Ads Quality team, with a speciality in machine learning. 
+            I am a Senior Software Engineer at Google on the Gmail Ads Quality team, with a speciality in machine learning. 
             I love teaching and am always looking for 
             opportunities to integrate teaching into my career journey. 
         </p>
